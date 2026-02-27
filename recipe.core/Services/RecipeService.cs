859ea96f3eff9c15a6f86093e939362ce8725324
fogelt <fogeltt@gmail.com>
2026-02-27 13:02:47 +0100

    typo

--- recipe.core/Services/RecipeService.cs
+++ recipe.core/Services/RecipeService.cs
@@ -1,45 +1,46 @@
-using recipe.core.DTOs;
 using recipe.core.Interfaces;
 using recipe.core.Models;
 
+namespace recipe.core.services;
+
 public class RecipeService : IRecipeService
 {
-    private readonly IGenericRepositry<Recipe> _repository;
+  private readonly IGenericRepository<Recipe> _repository;
 
-    public RecipeService(IGenericRepositry<Recipe> repository)
-    {
-        _repository = repository;
-    }
+  public RecipeService(IGenericRepository<Recipe> repository)
+  {
+    _repository = repository;
+  }
 
-    public async Task<IEnumerable<Recipe>> GetAllAsync()
-    {
-        return await _repository.GetAllAsync();
-    }
+  public async Task<IEnumerable<Recipe>> GetAllAsync()
+  {
+    return await _repository.GetAllAsync();
+  }
 
-    public async Task<Recipe?> GetByIdAsync(int id)
-    {
-        return await _repository.GetByIdAsync(id);
-    }
+  public async Task<Recipe?> GetByIdAsync(int id)
+  {
+    return await _repository.GetByIdAsync(id);
+  }
 
-    public Task<Recipe> CreateAsync(Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
-    public Task<bool> DeleteAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+  public Task<Recipe> CreateAsync(Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
+  public Task<bool> DeleteAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
 
-    public Task<IEnumerable<Recipe>> GetByDifficultyAsync(string level)
-    {
-        throw new NotImplementedException();
-    }
-    public Task<IEnumerable<Recipe>> SearchAsync(string term)
-    {
-        throw new NotImplementedException();
-    }
-    public Task<Recipe?> UpdateAsync(int id, Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
+  public Task<IEnumerable<Recipe>> GetByDifficultyAsync(string level)
+  {
+    throw new NotImplementedException();
+  }
+  public Task<IEnumerable<Recipe>> SearchAsync(string term)
+  {
+    throw new NotImplementedException();
+  }
+  public Task<Recipe?> UpdateAsync(int id, Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
 }
